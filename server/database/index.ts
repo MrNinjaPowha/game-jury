0a54e6719b11553c42876ec7d05293a86b03c63d
MrNinjaPowha <joakim.gietl@gmail.com>
2023-05-11 15:09:44 +0200

    Improved database connection

--- server/database/index.ts
+++ server/database/index.ts
@@ -1,15 +1,16 @@
 import mysql from 'mysql2/promise';
 
+const database = mysql.createConnection({
+  host: process.env.DBHost,
+  database: process.env.DBName,
+  user: process.env.DBUser,
+  password: process.env.DBPassword,
+});
+
 async function executeQuery(sql: string) {
   try {
-    const database = await mysql.createConnection({
-      host: process.env.DBHost,
-      database: process.env.DBName,
-      user: process.env.DBUser,
-      password: process.env.DBPassword,
-    });
-
-    return database.query(sql);
+    const connection = await database;
+    return connection.query(sql);
   } catch (err) {
     console.error(err);
     return [null];
